5a0db83209042f8f30b2eec527382403b74f12a2
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-04-10 00:33:25 +0100

    feat!: add compatibility for v4.3, more blender creative common films and scraper option to enable scraping youtube stream url

--- mov_cli_test/__init__.py
+++ mov_cli_test/__init__.py
@@ -15,4 +15,4 @@ plugin: PluginHookData = {
     }
 }
 
-__version__ = "1.1.1"
+__version__ = "1.1.3"
--- mov_cli_test/scraper.py
+++ mov_cli_test/scraper.py
@@ -6,24 +6,26 @@ if TYPE_CHECKING:
 
     from mov_cli import Config
     from mov_cli.http_client import HTTPClient
+    from mov_cli.scraper import ScraperOptionsT
 
 from pytube import YouTube
-from devgoldyutils import Colours, LoggerAdapter
+from devgoldyutils import Colours
 
-from mov_cli.logger import mov_cli_logger
 from mov_cli.utils import EpisodeSelector
-from mov_cli import Scraper, Series, Movie, Metadata, MetadataType
+from mov_cli import Scraper, Multi, Single, Metadata, MetadataType
 
 __all__ = ("TestScraper",)
 
-logger = LoggerAdapter(mov_cli_logger, prefix = "Test")
-
 class TestScraper(Scraper):
-    def __init__(self, config: Config, http_client: HTTPClient) -> None:
+    def __init__(self, config: Config, http_client: HTTPClient, options: Optional[ScraperOptionsT] = None) -> None:
         self.creative_common_films = [
-            Metadata(id = "https://youtu.be/aqz-KE-bpKQ", title = "Big Buck Bunny", type = MetadataType.MOVIE, year = "2008"),
-            Metadata(id = "https://www.youtube.com/watch?v=BBgghnQF6E4", title = "Steamboat Willie", type = MetadataType.MOVIE, year = "1928"),
-            Metadata(id = "https://cdn.devgoldy.xyz/ricky.webm", title = "Ricky :)", type = MetadataType.MOVIE, year = "2009")
+            Metadata(id = "https://youtu.be/aqz-KE-bpKQ", title = "Big Buck Bunny", type = MetadataType.SINGLE, year = "2008"),
+            Metadata(id = "https://www.youtube.com/watch?v=BBgghnQF6E4", title = "Steamboat Willie", type = MetadataType.SINGLE, year = "1928"),
+            Metadata(id = "https://cdn.devgoldy.xyz/ricky.webm", title = "Ricky :)", type = MetadataType.SINGLE, year = "2009"),
+            Metadata(id = "https://youtu.be/u9lj-c29dxI", title = "Wing It!", type = MetadataType.SINGLE, year = "2023"),
+            Metadata(id = "https://youtu.be/UXqq0ZvbOnk", title = "Charge", type = MetadataType.SINGLE, year = "2022"),
+            Metadata(id = "https://youtu.be/_cMxraX_5RE", title = "Sprite Fright", type = MetadataType.SINGLE, year = "2021"),
+            Metadata(id = "https://youtu.be/WhWc3b3KhnY", title = "Spring", type = MetadataType.SINGLE, year = "2019"),
         ]
 
         self.message = f"""
@@ -38,7 +40,7 @@ class TestScraper(Scraper):
   Then you can find the instructions on how to install a plugin over here: {Colours.PINK_GREY}https://github.com/mov-cli/mov-cli/wiki/Plugins{Colours.RESET}
         """
 
-        super().__init__(config, http_client)
+        super().__init__(config, http_client, options)
 
     def search(self, query: str, limit: int = None) -> Iterable[Metadata]:
         # This is where you would want to implement searching for your scraper. 
@@ -46,8 +48,8 @@ class TestScraper(Scraper):
 
         # ignore this
         # -------------
-        if query.lower() in ["abc", "all", "example"]:
-            logger.warning("This is an example/test plugin for mov-cli, press enter to skip if you aren't new to that.")
+        if query.lower() in ["abc", "all", "example", "query"]:
+            self.logger.warning("This is an example/test plugin for mov-cli, press enter to skip if you aren't new to that.")
             input(self.message + Colours.GREEN.apply("\n  Press ENTER to continue... "))
             for x in self.creative_common_films: yield x
 
@@ -70,19 +72,18 @@ class TestScraper(Scraper):
             if query.lower() in metadata.title.lower():
                 yield metadata # yield the element instead of appending it to a list for better performance and UX.
 
-    def scrape(self, metadata: Metadata, episode: Optional[EpisodeSelector] = None, **kwargs) -> Series | Movie:
-        if episode is None:
-            episode = EpisodeSelector()
+    def scrape(self, metadata: Metadata, episode: EpisodeSelector) -> Multi | Single:
+        scrape_stream_url = self.options.get("stream_url", False)
 
         url = metadata.id
 
-        if "https://youtu.be" in url:
+        if scrape_stream_url is True and "https://youtu.be" in url:
             url = YouTube(url).streams.get_highest_resolution().url
 
-        # NOTE: I could have just dropped series as all the media in my list are 
+        # NOTE: I could have just dropped multi as all the media in my list are 
         # films and not series but I'll leave it in here as an example.
         if metadata.type == MetadataType.SERIES:
-            return Series(
+            return Multi(
                 url = url, 
                 title = metadata.title, 
                 referrer = url, 
@@ -90,14 +91,13 @@ class TestScraper(Scraper):
                 subtitles = None
             )
 
-        return Movie(
+        return Single(
             url = url, 
             title = metadata.title, 
             referrer = url, 
-            year = metadata.year,
-            subtitles = None
+            year = metadata.year
         )
 
-    def scrape_episodes(self, metadata: Metadata, **kwargs) -> Dict[int | None, int]:
+    def scrape_episodes(self, metadata: Metadata) -> Dict[int | None, int]:
         # NOTE: Let's just return None for now as we don't have any series in the list hence no episodes.
         return {None: 1}
